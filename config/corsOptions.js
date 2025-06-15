5cf8586ba2f26d059c0ebc62b74c7fac033ea20c
Priyanshu Kumar <upiyush005@gmail.com>
2025-06-15 15:41:09 +0530

    fix CORS whitelist entry by removing trailing slash from URL

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,7 +4,7 @@ const whitelist = [
   // "http://localhost:5173",
   // "http://localhost:5174",
   // "http://localhost:5175",
-  "https://ever-basket.vercel.app/",
+  "https://ever-basket.vercel.app",
 ];
 const corsOptions = {
   origin: (origin, callback) => {
