e86c52fe2e4552f1200da570bbcdff639dfddda7
Priyanshu Kumar <upiyush005@gmail.com>
2025-06-27 11:13:41 +0530

    update CORS whitelist to include trailing slash for ever-basket URL

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -4,10 +4,13 @@ const whitelist = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
-  // "https://ever-basket.vercel.app",
+  "https://ever-basket.vercel.app/",
+  "https://ever-basket.vercel.app",
 ];
 const corsOptions = {
   origin: (origin, callback) => {
+    // console.log("CORS Origin:", origin);
+
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
